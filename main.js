672b38bc0849694fc3dcd875f68ad302cec64259
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-02 18:14:06 -0800

    cards

---
+++ main.js
@@ -0,0 +1,5 @@
+//menu button
+const menu = document.querySelector('#order')
+menu.addEventListener('click', () => {
+    window.open('./menu/index.html')
+})
---
+++ menu/main.js
@@ -0,0 +1,4 @@
+const title = document.querySelector('#name')
+title.addEventListener('click', () => {
+    window.open('../index.html')
+})
