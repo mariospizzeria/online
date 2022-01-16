f52d9c7739f4d89fcb280b19da56b797e23c49c8
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-15 22:19:39 -0800

    asdfawef

--- menu/customize/main.js
+++ menu/customize/main.js
@@ -1,5 +1,4 @@
 const storage = window.localStorage
-const toppings = {}
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 const title = document.querySelector('#name')
@@ -7,23 +6,12 @@ title.addEventListener('click', () => {
     window.open('../../index.html')
 })
 
-class topping {
-    constructor(topping) {
-        this.toppingName = topping
-    }
-
-    code(toppingName) {
-        var code = []
-        for (var i = 0; i < toppingName.split("").length; i++) {
-            for(var j = 0; j < alphabet.length; j++) {
-                if(toppingName.split("")[i] == alphabet[j]) {
-                    code.push(j+1)
-                }
-            }
+document.querySelectorAll('.topping').forEach(topping => {
+    topping.addEventListener('click', () => {
+        if(topping.classList.contains('redify')) {
+            topping.classList.toggle('redify')
+        } else {
+            topping.classList.toggle('redify')
         }
-        console.log(code.join(""))
-    }
-}
-
-const thing = new topping('pineapple')
-console.log(thing.code(thing.toppingName))
+    })
+})
