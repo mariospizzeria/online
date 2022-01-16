407e8c3d94598938df0a6438b9fa71613ecac5ce
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-15 22:24:46 -0800

    aaefafe

--- menu/main.js
+++ menu/main.js
@@ -2,3 +2,19 @@ const title = document.querySelector('#name')
 title.addEventListener('click', () => {
     window.open('../index.html')
 })
+
+const card1 = document.querySelector('#card1')
+const card2 = document.querySelector('#card2')
+const card3 = document.querySelector('#card3')
+
+card1.addEventListener('click', () => {
+    window.open('../menu/pizza')
+})
+
+card2.addEventListener('click', () => {
+    window.open('../menu/toppings')
+})
+
+card3.addEventListener('click', () => {
+    window.open('../menu/sides')
+})
